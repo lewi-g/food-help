2a21edf71bde42b7814888ac4be1c655a04125b0
lewi-g <dr.lewi@gmail.com>
2017-06-22 12:49:37 -0400

    food help initial

---
+++ food.js
@@ -0,0 +1,7 @@
+// Food Database requests
+//searching only within food database (not recipe analysis)
+// path = https://api.edamam.com/api/nutrition-data
+app_id =
+app_key =
+ingr =
+example of request "https://api.edamam.com/api/nutrition-data?app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&ingr=1%20large%20apple"
